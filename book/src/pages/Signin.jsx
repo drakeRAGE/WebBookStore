a419fa1a2cef39770239cba7f16635c08362b81b
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-03-24 20:24:16 +0530

    add redux toolkit

--- book/src/pages/Signin.jsx
+++ book/src/pages/Signin.jsx
@@ -1,10 +1,14 @@
 import {useState}  from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
+
 export default function SignIn() {
   const [formData, setFormData]= useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleChnage = (e) => {
     setFormData({
       ...formData,
@@ -16,8 +20,7 @@ export default function SignIn() {
     e.preventDefault();
     
     try {
-      // console.log('Form Submitted!');
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',  {
         method: 'POST',
         headers: {
@@ -28,21 +31,17 @@ export default function SignIn() {
       });
       const data = await res.json();
       if(data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      console.log(data);
+      dispatch(signInSuccess(data.message));
       navigate('/');
     } catch(error) {
-      setLoading(false);
-      setError(error.message);
+      signInFailure(e.message);
     }
 
   };
 
-  // console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
