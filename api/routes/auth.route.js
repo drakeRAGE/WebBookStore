ea5bbc5a11f18bebb4013dbb24e6fad9b5a1b0a7
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-03-24 19:25:43 +0530

    create sign-in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controllers.js';
+import { signup, signin } from '../controllers/auth.controllers.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
