b34d143528a1f90d046b69b40b9cad1235cf997c
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-03-24 11:35:26 +0530

    Create pages and routes with Header component

--- book/src/App.jsx
+++ book/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
