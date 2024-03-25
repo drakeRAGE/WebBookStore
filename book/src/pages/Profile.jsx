2dbff3e9e81692e0d27c5042d07700581d2cb30d
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-03-25 17:02:16 +0530

    complete profile page ui

--- book/src/pages/Profile.jsx
+++ book/src/pages/Profile.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+  const {currentUser} = useSelector((state) =>state.user);
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7 '>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={currentUser.avatar}  alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
+
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled-80'>Update</button>
+      </form>    
+
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
--- book/src/pages/Signin.jsx
+++ book/src/pages/Signin.jsx
@@ -31,7 +31,7 @@ export default function SignIn() {
         body: JSON.stringify(formData)
       });
       const data = await res.json();
-      console.log(data);
+      // console.log(data);
       if(data.success === false) {
         dispatch(signInFailure(data.message));
         return;
