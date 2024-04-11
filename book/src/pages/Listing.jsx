1b1cc3a0cba48076ed5dbbf70afb836f4302db62
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-04-11 19:23:20 +0530

    complete listing page

--- book/src/pages/Listing.jsx
+++ book/src/pages/Listing.jsx
@@ -1,64 +1,137 @@
-import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
-import {Swiper, SwiperSlide} from 'swiper/react'
-import SwiperCore from 'swiper'
-import {Navigation} from 'swiper/modules'
-import 'swiper/css/bundle'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+import {FaShare} from 'react-icons/fa';
+import { HiLightningBolt } from "react-icons/hi";
+import { VscDebugBreakpointData, VscAzureDevops } from "react-icons/vsc";
+import { SiAlpinelinux } from "react-icons/si";
+import { IoMan } from "react-icons/io5";
 
 export default function Listing() {
-    SwiperCore.use([Navigation]);
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const params = useParams();
 
-    const params = useParams();
-    const [listing, setListing]= useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    useEffect (()=> {
-        const fetchListing = async ()=> {
-            try {
-                setLoading(true);
-                const res = await fetch(`/api/listing/get/${params.listingId}`);
-                const data = await res.json();
-            
-                if(data.success === false){
-                    setError(true);
-                    setLoading(false);
-                    return;
-                }
-                setListing(data);
-                setLoading(false);
-                setError(false);
-            } catch (error) {
-                setError(true);
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
 
-        fetchListing();
-    }, [params.listingId])
   return (
     <main>
-        {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
-        {error && <p className='text-center my-7 text-2xl'>Something went wrong...</p>}
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+      {error && (
+        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className='h-[550px]'
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: 'cover',
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
+          <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+            <p className='text-2xl font-semibold'>
+              {listing.name} - ${' '} 
+              {listing.offer
+                ? listing.discountPrice.toLocaleString('en-US')
+                : listing.regularPrice.toLocaleString('en-US')}
+              {listing.type === 'rent' && ' / month'}
+            </p>
+            <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+                <IoMan className='text-lg'/>
+              {listing.author}
+            </p>
+            <div className='flex gap-4'>
+              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+              </p>
+              {listing.offer && (
+                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                </p>
+              )}
+            </div>
+            <p className='text-slate-800'>
+              <span className='font-semibold text-black'>Description - </span>
+              {listing.description}
+            </p>
+            <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <HiLightningBolt className='text-lg' />
+                {listing.Pages > 1
+                  ? `${listing.Pages} pages `
+                  : `${listing.Pages} page `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <VscDebugBreakpointData className='text-lg' />
 
-        {listing && !loading && !error && 
-        (<>
-            <Swiper navigation>
-                {listing.imageUrls.map((url)=> (
-                    <SwiperSlide key={url}>
-                        <div className='h-[550px]' style={
-                            {
-                            background: `url(${url}) center no-repeat`,
-                            backgroundSize: 'cover',
-                            }}
-                            
-                    
-                        ></div>
-                    </SwiperSlide>
-                )
-            )}
-            </Swiper>
-        </>
-        )}
+                {listing.Chapters > 1
+                  ? `${listing.Chapters} chapters `
+                  : `${listing.Chapters} chapter `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <SiAlpinelinux className='text-lg' />
+                {listing.BooksQuantity > 1
+                  ? `${listing.BooksQuantity} books `
+                  : `${listing.BooksQuantity} book `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <VscAzureDevops className='text-lg' />
+                {listing.published ? 'published' : 'Un-published'}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </main>
-  )
+  );
 }
