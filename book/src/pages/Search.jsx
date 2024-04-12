ce7daecd1ce60a4c06008d5b71cdbef5f414afdf
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-04-12 22:25:52 +0530

    add show more listings functionality

--- book/src/pages/Search.jsx
+++ book/src/pages/Search.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 import ListingItem from '../components/ListingItem';
 
+
+// bug -> The search with rent, sale and published is not working, but I think it will be fixed soon as the other types are working but I am currently some lazy so sorry for now
 export default function Search() {
     const navigate = useNavigate();
     const [sidebardata, setSidebardata] = useState({
@@ -15,6 +17,7 @@ export default function Search() {
     
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(()=> {
         const urlParams = new URLSearchParams(location.search)
@@ -43,10 +46,16 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
 
+            if(data.length > 8 ) {
+                setShowMore(true); 
+            } else {
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         };
@@ -91,6 +100,20 @@ export default function Search() {
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`)
     }
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+        setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+    };
 
   return (
     <div className='flex flex-col md:flex-row'>
@@ -158,6 +181,10 @@ export default function Search() {
                         <ListingItem key={listing._id} listing={listing} />
                     ))
                 }
+
+                {showMore && (
+                    <button className='text-green-700 hover:underline p-7 text-center w-full' onClick={onShowMoreClick}>Show more</button>
+                )}
             </div>
         </div>
     </div>
