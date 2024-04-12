7cc8f3eb671221f7d03941893851bcd94d5b4a14
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-04-12 16:00:22 +0530

    create search api route

--- api/controllers/listing.controllers.js
+++ api/controllers/listing.controllers.js
@@ -61,3 +61,47 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
+    }
+
+    let published = req.query.published;
+
+    if (published === undefined || published === 'false') {
+      published = { $in: [false, true] };
+    }
+
+    // let type = req.query.type;
+
+    // if (type === undefined || type === 'general') {
+    //   type = { $in: ['sale', 'rent'] };
+    // }
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      offer,
+      published,
+      // type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
