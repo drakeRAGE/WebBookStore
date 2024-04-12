17b11ad988db25416d06e78a6127adc5480bd996
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-04-12 22:51:41 +0530

    complete about page ui

--- book/src/pages/About.jsx
+++ book/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
+      <p className='mb-4 text-slate-700'>Dragbos Store is a leading online book selling platform that specializes in helping clients buying and selling books in the most desirable price. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our buyers and sellers achieve their reading goals by providing expert advice, personalized service, and a deep understanding of the global knowledge. Whether you are looking to buy or sell a book, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>We can a experience, knowledgable and interstful people from the current era which have the outstanding performance in literature.</p>
+    </div>
   )
 }
